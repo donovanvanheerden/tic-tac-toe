291a9e17898995ade42715b971c6b76a3a27cd48
Donovan van Heerden <dp.vanheerden@gmail.com>
2021-04-29 17:20:05 +0200

    domain exceptions

---
+++ TicTacToe.Core/Domain/Exceptions/InvalidCell.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Core.Domain.Exceptions
+{
+    public class InvalidCell : Exception
+    {
+        public InvalidCell() : base("Invalid Cell") { }
+        public InvalidCell(string message = "Invalid Cell") : base(message) { }
+    }
+}
---
+++ TicTacToe.Core/Domain/Exceptions/NotFound.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Core.Domain.Exceptions
+{
+    public class NotFound : Exception
+    {
+        public NotFound() : base("Resource not found") { }
+        public NotFound(string message = "Resource not found") : base(message) { }
+    }
+}
