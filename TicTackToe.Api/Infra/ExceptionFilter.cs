f5fbcc923dd23b399740c3b8afc4e2a6182fd172
Donovan van Heerden <dp.vanheerden@gmail.com>
2021-04-29 17:20:29 +0200

    filter exception, need to add custom status codes for domain exceptions

---
+++ TicTackToe.Api/Infra/ExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTackToe.Api.Infra
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        public ExceptionFilter()
+        { }
+
+        public override void OnException(ExceptionContext context)
+        {
+            Log.Error(context.Exception, "Exception Filter");
+
+            context.HttpContext.Response.ContentType = "application/json";
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new JsonResult(new
+            {
+                error = new[] { context.Exception.Message },
+                stackTrace = context.Exception.StackTrace
+            });
+        }
+    }
+}
